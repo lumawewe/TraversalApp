7c01093ba40bd9f4968988b2d8b1150ec1c732dc
bakitchy <bakitchy@gmail.com>
2024-01-16 08:56:49 +0300

    ...

---
+++ Traversal.Core/Concrete/About2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traversal.Core.Concrete
+{
+    public class About2
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Title2 { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public bool Status { get; set; }
+       
+    }
+}

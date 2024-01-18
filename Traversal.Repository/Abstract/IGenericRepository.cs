765892fab2d711af9b0a9afc65720b351ded7d0e
bakitchy <bakitchy@gmail.com>
2024-01-18 09:50:30 +0300

    ...

---
+++ Traversal.Repository/Abstract/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traversal.Repository.Abstract
+{
+    public interface IGenericRepository<T>
+    {
+        void Add(T t);
+        void Update(T t);
+        void Delete(T t);
+
+        List<T> GetList();
+        List<T> GetListByFilter(Expression<Func<T, bool>> filter);
+
+        T GetById(int id);
+
+    }
+}
---
+++ Traversal.Repository/Repository/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Traversal.Repository.Abstract;
+using Traversal.Repository.Concrete;
+
+namespace Traversal.Repository.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public void Add(T t)
+        {
+            using var c = new Context();
+            c.Add(t);
+            c.SaveChanges();
+        }
+
+        public void Delete(T t)
+        {
+            using var c = new Context();
+            c.Remove(t);
+            c.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            using var c = new Context();
+            return c.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetListByFilter(System.Linq.Expressions.Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T t)
+        {
+            using var c = new Context();
+            c.Update(t);
+            c.SaveChanges();
+        }
+    }
+}

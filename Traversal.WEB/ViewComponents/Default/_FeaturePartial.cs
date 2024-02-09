92ce3c39eb38b8fae1316f54b20edea798c0e6ee
bakitchy <bakitchy@gmail.com>
2024-02-09 14:55:11 +0300

    ...

---
+++ Traversal.WEB/ViewComponents/Default/_FeaturePartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Traversal.Repository.EntityFramework;
+using Traversal.Service.Concrete;
+
+namespace Traversal.WEB.ViewComponents.Default
+{
+    public class _FeaturePartial : ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.TGetList();
+            return View(values); 
+        }
+    }
+}
--- Traversal.WEB/ViewComponents/Default/_StatsPartial.cs
+++ Traversal.WEB/ViewComponents/Default/_StatsPartial.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Traversal.Repository.Concrete;
 
 namespace Traversal.WEB.ViewComponents.Default
 {
     public class _StatsPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        public IViewComponentResult Invoke() 
+        {
+            using var c = new Context();
+            ViewBag.v1 = c.Destinations.Count();
+            ViewBag.v2 = c.Guides.Count();
+            ViewBag.v3 = "285";
+            return View(); 
+        }
 
     }
 }

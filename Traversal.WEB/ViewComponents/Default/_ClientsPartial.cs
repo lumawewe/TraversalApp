295252fd793814a143b94603bc506780d54101ba
bakitchy <bakitchy@gmail.com>
2024-02-12 13:12:19 +0300

    ...

---
+++ Traversal.WEB/ViewComponents/Default/_ClientsPartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Traversal.Repository.EntityFramework;
+using Traversal.Service.Concrete;
+
+namespace Traversal.WEB.ViewComponents.Default
+{
+    public class _ClientsPartial : ViewComponent
+    {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = testimonialManager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ Traversal.WEB/ViewComponents/Default/_SubAboutsPartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Traversal.Repository.EntityFramework;
+using Traversal.Service.Concrete;
+
+namespace Traversal.WEB.ViewComponents.Default
+{
+    public class _SubAboutsPartial : ViewComponent
+    {
+        SubAboutManager subAboutManager = new SubAboutManager(new EfSubAboutRepository());
+        
+        public IViewComponentResult Invoke() 
+        {
+            var values = subAboutManager.TGetList();
+            return View(values); 
+        }
+    }
+}

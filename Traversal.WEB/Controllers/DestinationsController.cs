f2c0611f524afdc40b540c972f129fc91da311ee
bakitchy <bakitchy@gmail.com>
2024-02-13 13:14:14 +0300

    ...

---
+++ Traversal.WEB/Controllers/DestinationsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Traversal.Repository.EntityFramework;
+using Traversal.Service.Concrete;
+
+namespace Traversal.WEB.Controllers
+{
+    public class DestinationsController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationRepository());
+        
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}

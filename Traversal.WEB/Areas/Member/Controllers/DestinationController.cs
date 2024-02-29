e06cc6637ef29d431b5df28764c7de24ca66c8d9
bakitchy <bakitchy@gmail.com>
2024-02-29 15:55:54 +0300

    ..

---
+++ Traversal.WEB/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Traversal.Repository.EntityFramework;
+using Traversal.Service.Concrete;
+
+namespace Traversal.WEB.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationRepository());
+
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}

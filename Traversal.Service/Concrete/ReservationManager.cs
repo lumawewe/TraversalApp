1270fd4facb1072a5c41525fd846a7e4f3f55e12
bakitchy <bakitchy@gmail.com>
2024-03-06 13:29:24 +0300

    ....

---
+++ Traversal.Service/Concrete/ReservationManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Traversal.Core.Concrete;
+using Traversal.Repository.Abstract;
+using Traversal.Service.Abstract;
+
+namespace Traversal.Service.Concrete
+{
+    public class ReservationManager : IReservationService
+    {
+        IReservationRepository _reservationsRepository;
+
+        public ReservationManager(IReservationRepository reservationsRepository)
+        {
+            _reservationsRepository = reservationsRepository;
+        }
+
+        public void TAdd(Reservation t)
+        {
+            _reservationsRepository.Add(t);
+        }
+
+        public void TDelete(Reservation t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Reservation t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Traversal.WEB/Areas/Member/Controllers/ReservationsController.cs
+++ Traversal.WEB/Areas/Member/Controllers/ReservationsController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Traversal.Core.Concrete;
+using Traversal.Repository.EntityFramework;
+using Traversal.Service.Concrete;
 
 namespace Traversal.WEB.Areas.Member.Controllers
 {
@@ -9,23 +12,24 @@ namespace Traversal.WEB.Areas.Member.Controllers
     [AllowAnonymous]
     public class ReservationsController : Controller
     {
-        private readonly UserManager<AppUser> _userManager;
-
-        public ReservationsController(UserManager<AppUser> userManager)
-        {
-            _userManager = userManager;
-        }
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationRepository());
+        ReservationManager reservationManager = new ReservationManager(new EfReservationRepository());
 
         [HttpGet]
         public IActionResult NewReservation()
         {
+            List<SelectListItem> values = (from x in destinationManager.TGetList()
+                                           select new SelectListItem 
+                                           { Text = x.City , Value = x.Id.ToString()}).ToList();
+            ViewBag.v = values;
             return View();
         }
 
         [HttpPost]
         public IActionResult NewReservation(Reservation p)
         {
-            return View();
+            reservationManager.TAdd(p);
+            return RedirectToAction("MyCurrentReservation");
         }
 
         public IActionResult MyCurrentReservation()

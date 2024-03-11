4efbec950d8761f2c20c30dc52303f7536fae319
bakitchy <bakitchy@gmail.com>
2024-03-11 16:03:28 +0300

    ...

---
+++ Traversal.WEB/Areas/Member/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Traversal.Core.Concrete;
+
+namespace Traversal.WEB.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public DashboardController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userName = values.Name + " " + values.Surname;
+            ViewBag.userImage = values.ImageUrl;
+            return View();
+        }
+    }
+}

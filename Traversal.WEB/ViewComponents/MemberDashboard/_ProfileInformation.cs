28a2de07f2d7f9897d077af3d21f3096a7a4d48d
bakitchy <bakitchy@gmail.com>
2024-03-12 13:19:20 +0300

    ...

---
+++ Traversal.WEB/ViewComponents/MemberDashboard/_ProfileInformation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.View;
+using Traversal.Core.Concrete;
+
+namespace Traversal.WEB.ViewComponents.MemberDashboard
+{
+    public class _ProfileInformation : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _ProfileInformation(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            
+            ViewBag.memberName = values.UserName + " " + values.Surname;
+            ViewBag.memberPhone = values.PhoneNumber;
+            ViewBag.memberEmail = values.Email;
+
+            return View();
+        }
+    }
+}

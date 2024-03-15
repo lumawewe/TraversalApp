a9579c078446b552b7c815a108508be59cd5e5a0
bakitchy <bakitchy@gmail.com>
2024-03-15 20:14:33 +0300

    admin themes and panel added to makes configurations.

---
+++ Traversal.WEB/Areas/Admin/Controllers/DestinationsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Traversal.Core.Concrete;
+using Traversal.Repository.EntityFramework;
+using Traversal.Service.Abstract;
+using Traversal.Service.Concrete;
+
+namespace Traversal.WEB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class DestinationsController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationRepository());
+
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddDestination(Destination d)
+        {
+            destinationManager.TAdd(d);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateDestination()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination d)
+        {
+            return View();
+        }
+
+        public IActionResult DeleteDestination()
+        {
+            return View();  
+        }
+
+    }
+}
---
+++ Traversal.WEB/Controllers/AdminsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.WEB.Controllers
+{
+    public class AdminsController : Controller
+    {
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AppBrandDemoPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult MenuPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult ScriptPartial()
+        {
+            return PartialView();
+        }
+    }
+}
